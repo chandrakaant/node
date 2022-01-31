60d7023d9aa1a8912d72a76d56f5697ee434a7c0
chandrakantpatel <chandrakantpatel23@gmail.com>
2022-01-31 20:42:39 +0530

    Added dotenv file and package secured DB connection link

--- Task_Manager/app.js
+++ Task_Manager/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
+require('dotenv').config()
 
 var util= require('util');
 var encoder = new util.TextEncoder('utf-8');
@@ -24,7 +25,7 @@ const port = 3000;
 
 const start = async () => {
     try {
-        await connectDB()
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server started running on port ${port}`));
     } catch (error) {
         console.log(error)
--- Task_Manager/db/connect.js
+++ Task_Manager/db/connect.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose')
 
-const connectionString = 'mongodb+srv://chandra:TestingUser1234@cluster0.94lsl.mongodb.net/taskmanager?retryWrites=true&w=majority'
-
-const connectDB = (uri) => {
-    mongoose.connect(connectionString)
+const connectDB = (url) => {
+    mongoose.connect(url)
 }
 
 module.exports = connectDB

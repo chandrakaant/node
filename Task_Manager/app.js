5b661d59f5aa5b055fd42266ed0e7e5d9e12f609
chandrakantpatel <chandrakantpatel23@gmail.com>
2022-01-31 20:30:40 +0530

    Added file to connect to mongo DB

--- Task_Manager/app.js
+++ Task_Manager/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+
+var util= require('util');
+var encoder = new util.TextEncoder('utf-8');
 
 app.use(express.static('./public'))
 
@@ -16,8 +20,15 @@ app.get('/hello', (req, res)=>{
 
 app.use('/api/v1/tasks', tasks);
 
-
-
-
 const port = 3000;
-app.listen(port, console.log(`Server started running on port ${port}`));
+
+const start = async () => {
+    try {
+        await connectDB()
+        app.listen(port, console.log(`Server started running on port ${port}`));
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
--- Task_Manager/db/connect.js
+++ Task_Manager/db/connect.js
@@ -1,1 +1,9 @@
-const connectionString = 'mongodb+srv://chandra:<password>@cluster0.94lsl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const mongoose = require('mongoose')
+
+const connectionString = 'mongodb+srv://chandra:TestingUser1234@cluster0.94lsl.mongodb.net/taskmanager?retryWrites=true&w=majority'
+
+const connectDB = (uri) => {
+    mongoose.connect(connectionString)
+}
+
+module.exports = connectDB

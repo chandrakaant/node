48dfb78c98057846a1e2924aa454263f6367ac6e
chandrakantpatel <chandrakantpatel23@gmail.com>
2022-02-15 20:45:58 +0530

    Filters, Sorting, Fields and Custom parameters

--- Store_api/controllers/products.js
+++ Store_api/controllers/products.js
@@ -2,7 +2,61 @@ const Product = require('../models/product')
 
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find(res.query);
+    const {featured, company, name, sort, fields,numericFilter} = req.query;
+    const queryObject = {}
+    if(featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+    if(company) {
+        queryObject.company = company;
+    }
+    if(name) {
+        queryObject.name = {$regex: name, $options: 'i'};
+    }
+
+    let result = Product.find(queryObject)
+
+    if(sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createdDate')
+    }
+
+    if(fields) {
+        const fieldList = fields.split(',').join(' ')
+        result = result.select(fieldList)
+    }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit;
+
+    if(numericFilter) {
+        const operatorMap = {
+            '>' : '$gt',
+            '>=' : '$gte',
+            '=' : '$eq',
+            '<' : '$lt',
+            '<=' : '$lte'
+        }
+
+        const regex = /\b(<|>|>=|<=|=)\b/g
+        let filters = numericFilter.replace(regex, (match) => `-${operatorMap[match]}-`)
+        const options = ['price', 'rating'];
+        filters = filters.split(',').forEach((item) => {
+            const[field,operator,value] = item.split('-');
+            if(options.includes(field)) {
+                queryObject[field] = {[operator]: Number(value)}
+            }
+        })
+        console.log(filters);
+    }
+
+    result = result.skip(skip).limit(limit)
+
+    console.log(queryObject);
+    const products = await result;
     res.status(200).json({products, nbHits: products.length})
 }
 
